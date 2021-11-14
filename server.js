b60aaf79a98aa1dec9238cc37e93cbb018256da2
Zach Smith <Lemelisk27@gmail.com>
2021-11-14 13:45:00 -0800

    added a under constructin landing page

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes)
+app.use(express.static("public"));
 
 sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
