5df5f23452f6a4945476cd7a5da2b1620627f98c
Zach Smith <Lemelisk27@gmail.com>
2021-11-23 19:27:41 -0800

    added login page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -18,4 +18,38 @@ router.get("/",(req,res)=>{
     })
 })
 
+router.post("/login", (req,res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+    .then(foundUser =>{
+        if(!foundUser) {
+            res.status(401).json({Message:"Incorrect Username or Password"})
+        }
+        else if(bcrypt.compareSync(req.body.password,foundUser.password)) {
+            const token = jwt.sign({
+                username:foundUser.username,
+                id:foundUser.id
+            },
+            process.env.JWT_SECRET
+            ,{
+                expiresIn:"2h"
+            })
+            res.json({
+                token:token,
+                user:foundUser
+            })
+        }
+        else {
+            res.status(401).json({Message: "Incorrect Username or Password"})
+        }
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
 module.exports = router
