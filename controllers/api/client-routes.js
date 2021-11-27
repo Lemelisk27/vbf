a777a5bfe2c1525268bade2f8d21428103d297ee
Zach Smith <Lemelisk27@gmail.com>
2021-11-26 18:15:43 -0800

    added client routes

--- controllers/api/client-routes.js
+++ controllers/api/client-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {Client,Animal,Clinic} = require("../../models")
 const sequelize = require('../../config/connection')
+const tokenAuth = require("../../middleware/tokenAuth")
 
 router.get("/",(req,res)=>{
     Client.findAll({
@@ -13,6 +14,29 @@ router.get("/",(req,res)=>{
     .catch(err=>{
         console.log(err)
         res.status(500).json(err)
+    })
+})
+
+router.get("/all", tokenAuth, (req, res) => {
+    Client.findAll({
+        order: ["first_name"],
+        attributes: ["id", [sequelize.fn("concat", sequelize.col('first_name'), " ", sequelize.col('last_name')), "full_name"], "phone"],
+        include: [{
+            model: Animal,
+            attributes: ["id", "name"]
+        }]
+    })
+    .then(clients=>{
+        if(clients) {
+            res.json(clients)
+        }
+        else {
+            res.status(404).json({Message: "No Clients Found"})
+        }
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
     })
 })
 
