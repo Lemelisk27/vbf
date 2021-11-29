2fdc452aa6a721585dd87bc78f3ec8543a5ef168
Zach Smith <Lemelisk27@gmail.com>
2021-11-29 14:04:16 -0800

    added more inventory seeds

--- seeds/Inventoryitems-seeds.js
+++ seeds/Inventoryitems-seeds.js
@@ -35,6 +35,97 @@ const inventoryItemsData = [
         cost: 4.20,
         qty: 100,
         UnitId: 2
+    },
+    {
+        id: 6,
+        item_name: "FeLv/FIV Test",
+        cost: 21.00,
+        qty: 10,
+        UnitId: 3
+    },
+    {
+        id: 7,
+        item_name: "Heartworm Test Kit",
+        cost: 21.00,
+        qty: 10,
+        UnitId: 3
+    },
+    {
+        id: 8,
+        item_name: "Euthosol",
+        cost: 0.6,
+        qty: 50,
+        UnitId: 2
+    },
+    {
+        id: 9,
+        item_name: "Ketamine",
+        cost: 1.8,
+        qty: 5,
+        UnitId: 2
+    },
+    {
+        id: 10,
+        item_name: "Morphine",
+        cost: 9,
+        qty: 20,
+        UnitId: 1
+    },
+    {
+        id: 11,
+        item_name: "Telazol",
+        cost: 4.85,
+        qty: 100,
+        UnitId: 2
+    },
+    {
+        id: 12,
+        item_name: "Torbugesic",
+        cost: 18.16,
+        qty: 10,
+        UnitId: 2
+    },
+    {
+        id: 13,
+        item_name: "Acepromazine",
+        cost: 0.41,
+        qty: 50,
+        UnitId: 2
+    },
+    {
+        id: 14,
+        item_name: "Dexamethasone",
+        cost: 0.13,
+        qty: 50,
+        UnitId: 2
+    },
+    {
+        id: 15,
+        item_name: "Isoflurane",
+        cost: 11.45,
+        qty: 10,
+        UnitId: 1
+    },
+    {
+        id: 16,
+        item_name: "Atropine",
+        cost: 0.21,
+        qty: 50,
+        UnitId: 2
+    },
+    {
+        id: 17,
+        item_name: "Epinephrine",
+        cost: 2.77,
+        qty: 1,
+        UnitId: 3
+    },
+    {
+        id: 18,
+        item_name: "Solu Delta Cortef",
+        cost: 9.25,
+        qty: 5,
+        UnitId: 3
     }
 ]
 
--- seeds/inventoryjoin-seeds.js
+++ seeds/inventoryjoin-seeds.js
@@ -25,6 +25,66 @@ const inventoryJoinData = [
         id: 5,
         InventoryId: 1,
         InventoryitemsId: 5
+    },
+    {
+        id: 6,
+        InventoryId: 2,
+        InventoryitemsId: 6
+    },
+    {
+        id: 7,
+        InventoryId: 2,
+        InventoryitemsId: 7
+    },
+    {
+        id: 8,
+        InventoryId: 4,
+        InventoryitemsId: 8
+    },
+    {
+        id: 9,
+        InventoryId: 4,
+        InventoryitemsId: 9
+    },
+    {
+        id: 10,
+        InventoryId: 4,
+        InventoryitemsId: 10
+    },
+    {
+        id: 11,
+        InventoryId: 4,
+        InventoryitemsId: 11
+    },
+    {
+        id: 12,
+        InventoryId: 4,
+        InventoryitemsId: 12
+    },
+    {
+        id: 13,
+        InventoryId: 5,
+        InventoryitemsId: 13
+    },
+    {
+        id: 14,
+        InventoryId: 5,
+        InventoryitemsId: 14
+    },
+    {
+        id: 15,
+        InventoryId: 5,
+        InventoryitemsId: 15
+    },
+    {
+        id: 16,
+        InventoryId: 6,
+        InventoryitemsId: 16
+    },
+    {
+        id: 17,
+        InventoryId: 6,
+        InventoryitemsId: 17
     }
 ]
 
