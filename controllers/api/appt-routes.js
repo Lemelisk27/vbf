f6535a3e3f739761c1f737baf0e232a4ab005668
Zach Smith <Lemelisk27@gmail.com>
2021-11-28 16:59:49 -0800

    added calendar routes

--- controllers/api/appt-routes.js
+++ controllers/api/appt-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {Appt, Animal} = require("../../models")
+const tokenAuth = require("../../middleware/tokenAuth")
 
 router.get("/",(req,res)=>{
     Appt.findAll({
@@ -8,6 +9,22 @@ router.get("/",(req,res)=>{
     })
     .then(apptData=>{
         res.json(apptData)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+router.get("/all", tokenAuth, (req, res) => {
+    Appt.findAll()
+    .then(apptData=>{
+        if(apptData) {
+            res.json(apptData)
+        }
+        else {
+            res.status(404).json({Message: "No Appointments Found"})
+        }
     })
     .catch(err=>{
         console.log(err)
--- seeds/appt-seeds.js
+++ seeds/appt-seeds.js
@@ -4,24 +4,24 @@ const apptData = [
     {
         id: 1,
         title: "Nail Trim",
-        start_date: new Date(2021, 11, 1, 1, 30),
-        end_date: new Date(2021, 11, 1, 2, 0),
+        start_date: new Date(2021, 10, 28, 1, 30),
+        end_date: new Date(2021, 10, 28, 2, 0),
         notes: "Really needs this",
         AnimalId: 1
     },
     {
         id: 2,
         title: "Annual Exam",
-        start_date: new Date(2021, 11, 1, 2, 30),
-        end_date: new Date(2021, 11, 1, 3, 0),
+        start_date: new Date(2021, 10, 28, 2, 30),
+        end_date: new Date(2021, 10, 28, 3, 0),
         notes: "Make sure nose is cold and wet",
         AnimalId: 2
     },
     {
         id: 3,
         title: "Lump Eval",
-        start_date: new Date(2021, 11, 1, 3, 15),
-        end_date: new Date(2021, 11, 1, 3, 45),
+        start_date: new Date(2021, 10, 28, 3, 15),
+        end_date: new Date(2021, 10, 28, 3, 45),
         notes: "Lump isn't anything but owner wants it checked.",
         AnimalId: 3
     }
