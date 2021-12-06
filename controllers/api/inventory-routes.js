d87ad496a49041de0f6915b510f02d62be5f4746
Zach Smith <Lemelisk27@gmail.com>
2021-12-06 00:31:57 -0800

    added more inventory routes

--- controllers/api/inventory-routes.js
+++ controllers/api/inventory-routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {Inventory,Inventoryitems,Unit,Inventoryjoin} = require("../../models")
 const sequelize = require('../../config/connection')
-const tokenAuth = require("../../middleware/tokenAuth")
+const tokenAuth = require("../../middleware/tokenAuth");
+const e = require('express');
 
 router.get("/",(req,res)=>{
     if(!req.session.user){
@@ -134,6 +135,125 @@ router.delete("/categories/:id", tokenAuth, (req, res) => {
     })
     .then(deletedCategory=>{
         res.json(deletedCategory)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.get("/units", tokenAuth, (req, res) => {
+    Unit.findAll({
+        order: ["unit_name"]
+    })
+    .then(foundUnit=>{
+        if (foundUnit) {
+            res.json(foundUnit)
+        }
+        else {
+            res.status(404).json({Message: "Nothing Found"})
+        }
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.post("/items", tokenAuth, (req, res) => {
+    Inventoryitems.create({
+        item_name: req.body.item_name,
+        cost: req.body.cost,
+        qty: req.body.qty,
+        UnitId: req.body.UnitId
+    })
+    .then(createdItem=>{
+        res.json(createdItem)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.post("/joins", tokenAuth, (req, res) => {
+    Inventoryjoin.create({
+        InventoryId: req.body.InventoryId,
+        InventoryitemsId: req.body.InventoryitemsId
+    })
+    .then(createdJoin=>{
+        res.json(createdJoin)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.get("/joins", tokenAuth, (req, res) => {
+    Inventoryjoin.findAll()
+    .then(foundJoins=>{
+        if(foundJoins) {
+            res.json(foundJoins)
+        }
+        else {
+            res.status(404).json({Message: "Nothing Found"})
+        }
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.put("/joins", tokenAuth, (req, res) => {
+    Inventoryjoin.update({
+        InventoryId: req.body.InventoryId,
+        InventoryitemsId: req.body.InventoryitemsId
+    },
+    {
+        where: {
+            id: req.body.id
+        }
+    })
+    .then(updatedJoin=>{
+        res.json(updatedJoin)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.put("/items", tokenAuth, (req, res) => {
+    Inventoryitems.update({
+        item_name: req.body.item_name,
+        cost: req.body.cost,
+        qty: req.body.qty,
+        UnitId: req.body.UnitId
+    },
+    {
+        where: {
+            id: req.body.id
+        }
+    })
+    .then(updatedItem=>{
+        res.json(updatedItem)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({Message: "An Error Occured", err:err})
+    })
+})
+
+router.delete("/items/:id", tokenAuth, (req, res) => {
+    Inventoryitems.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(deletedItem=>{
+        res.json(deletedItem)
     })
     .catch(err=>{
         console.log(err)
